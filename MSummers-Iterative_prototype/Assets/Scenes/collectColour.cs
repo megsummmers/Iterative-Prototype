55463550187068940d5f20865a9a15719e20f335
megsummmers <70597858+megsummmers@users.noreply.github.com>
2022-03-25 10:33:20 -0400

    Update: Added animation
    Added final animation of the walls moving down once the player hits the collectible

--- MSummers-Iterative_prototype/Assets/Scenes/collectColour.cs
+++ MSummers-Iterative_prototype/Assets/Scenes/collectColour.cs
@@ -7,8 +7,9 @@ public class collectColour : MonoBehaviour
   //public GameObject mazeWalls;
   public GameObject redGem;
   public Material redMat;
-  private GameObject[] walls;
-  bool anim = false;
+  public MeshRenderer gemRend;
+  public GameObject walls;
+  private bool anim = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,30 +20,33 @@ public class collectColour : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-
+      if(anim){
+        Debug.Log(":D");
+        walls.transform.Translate(0, -0.1f, 0);
+      }
     }
 
     void OnTriggerEnter(Collider other)
     {
       if(other.CompareTag("Player")){
         //hides object
-        gameObject.SetActive(false);
+        gemRend.enabled = false;
         //change gem colour
         redGem.GetComponent<Renderer>().material = redMat;
-        Debug.Log(":D");
         anim = true;
         //move walls
-        if(anim == true){
-          Debug.Log(":D");
-          walls = GameObject.FindGameObjectsWithTag("Movable");
-          foreach(GameObject w in walls){
-            w.transform.Translate (Vector3.down * Time.deltaTime, Space.World);
-            float posY = gameObject.transform.position.y;
-            if (posY <= -8){
-              anim = false;
-            }
-          }
-        }
+
+        // if(anim == true){
+        //   Debug.Log(":D");
+        //   walls = GameObject.FindGameObjectsWithTag("Movable");
+        //   foreach(GameObject w in walls){
+        //     w.transform.Translate (Vector3.down * Time.deltaTime, Space.World);
+        //     float posY = gameObject.transform.position.y;
+        //     if (posY <= -8){
+        //       anim = false;
+        //     }
+        //   }
+        // }
       }
     }
 }
